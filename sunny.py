f43f905a3dff73c66433143c82ec6c97b912ee08
Rashied Imambaks <r@imambaks.nl>
2016-05-09 10:52:45 +0200

    empty the readme

--- sunny.py
+++ sunny.py
@@ -4,9 +4,25 @@ import json
 from waitress import serve
 from falcon_cors import CORS
 import numpy as np
+import pandas as pd
 
 cors = CORS(allow_origins_list=['http://localhost:3000/', 'http://localhost:*', 'http://localhost:3000'])
 
+
+df = pd.read_hdf('log_relatics.h5')
+
+
+def get_year(parameter,*args,freq: str='W') -> pd.DataFrame:
+    if args:
+        df2 = df[parameter:args[0]]
+    else:
+        df2 = df[parameter]
+
+    name = df2.groupby(pd.Grouper(freq=freq))['user_name'].unique()
+    return name
+
+
+names = (get_year('2015-2','2015-7').to_json(date_format='epoch'))
 
 class QuoteResource:
     def on_get(self, req, resp):
